3c267e171960598ca577600d039b4daab0caf880
Lukas Calvo <lukascalvo2022@gmail.com>
2026-07-29 21:18:52 -0400

    octavo commit: agregado el boton de catalogo y notificaciones, notificaciones quedara como modal

--- js/componentes/barra-nav.js
+++ js/componentes/barra-nav.js
@@ -6,9 +6,18 @@ class BarraNav extends HTMLElement {
           <img src="img/home.svg" alt="Página principal" width="40" height="40">
           <span class="tooltip">Página Principal</span>
         </a>
+
+        <a href="catalogo.html" id="btn-catalogo" class="btn-icono">
+          <img src="img/catalog.svg" alt="Página principal" width="40" height="40">
+          <span class="tooltip">Catálogo</span>
+        </a>
       </div>
 
       <div id="barra-acciones-usuario" class="barra-acciones">
+        <button id="btn-notis" class="btn-icono">
+          <img src="img/user-notificaciones.svg" alt="Notificaciones" width="40" height="40">
+          <span class="tooltip">Notificaciones</span>
+        </button>
         <a href="carrito.html" id="btn-carrito" class="btn-icono">
           <img src="img/user-carrito.svg" alt="Carrito de compras" width="40" height="40">
           <span class="tooltip">Carrito de compras</span>
