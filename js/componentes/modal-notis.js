b9687cbf2e1491371380be32b564456a8a59d7b6
Lukas Calvo <lukascalvo2022@gmail.com>
2026-08-04 10:07:08 -0400

    decimo commit: sistema de notificaciones agregado para que tenga un mensaje por defecto en el caso que no haya

--- js/componentes/modal-notis.js
+++ js/componentes/modal-notis.js
@@ -1,9 +1,24 @@
 class ModalNotis extends HTMLElement {
     connectedCallback() {
+        this.notificaciones = [];
+
+        this.render();
+    }
+
+    render() {
+        const hayNotificaciones = this.notificaciones.length > 0;
+
+        const contenidoLista = hayNotificaciones
+            ? this.notificaciones.map(n => `<div class="notificacion-item">${n}</div>`).join('')
+            : `<p class="sin-notificaciones">No tienes notificaciones</p>`;
+
         this.innerHTML = `
             <div id="modal-notis" class="modal oculto">
-              <div id="modal-notis-contenido"class="modal-contenido">
+              <div id="modal-notis-contenido" class="modal-contenido">
                 <h3>Notificaciones</h3>
+                <div id="lista-notificaciones">
+                  ${contenidoLista}
+                </div>
               </div>
             </div>
             `
